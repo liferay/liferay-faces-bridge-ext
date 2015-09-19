9d16349b0ae4a84b7336a1a9e56950625203ea55
Neil Griffin <neil.griffin.scm@gmail.com>
2015-09-19 13:42:06 -0400

    FACES-2452 Split the Liferay Faces multi-module Maven project into independently versioned Maven projects (source formatting)

--- bridge-ext/src/main/java/com/liferay/faces/bridge/liferay/internal/WindowStateValidatorFactoryLiferayImpl.java
+++ bridge-ext/src/main/java/com/liferay/faces/bridge/liferay/internal/WindowStateValidatorFactoryLiferayImpl.java
@@ -1,10 +1,25 @@
+/**
+ * Copyright (c) 2000-2015 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 package com.liferay.faces.bridge.liferay.internal;
-
 
 import com.liferay.faces.bridge.WindowStateValidator;
 import com.liferay.faces.bridge.WindowStateValidatorFactory;
 
 
+/**
+ * @author  Neil Griffin
+ */
 public class WindowStateValidatorFactoryLiferayImpl extends WindowStateValidatorFactory {
 
 	// Private Data Members
@@ -18,6 +33,7 @@ public class WindowStateValidatorFactoryLiferayImpl extends WindowStateValidator
 	public WindowStateValidator getWindowStateValidator() {
 
 		WindowStateValidator wrappedWindowStateValidator = wrappedWindowStateValidatorFactory.getWindowStateValidator();
+
 		return new WindowStateValidatorLiferayImpl(wrappedWindowStateValidator);
 	}
 
