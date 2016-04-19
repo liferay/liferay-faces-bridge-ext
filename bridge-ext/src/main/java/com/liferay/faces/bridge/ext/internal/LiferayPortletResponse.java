c2adabdee7e417e4a470f48beab48f0b789a3f07
Neil Griffin <neil.griffin.scm@gmail.com>
2016-04-19 17:39:57 -0400

    FACES-2637 Move BridgeURL to com.liferay.faces.bridge and simplify the API by moving implementation details to the bridge implementation

--- bridge-ext/src/main/java/com/liferay/faces/bridge/ext/internal/LiferayPortletResponse.java
+++ bridge-ext/src/main/java/com/liferay/faces/bridge/ext/internal/LiferayPortletResponse.java
@@ -11,7 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-package com.liferay.faces.bridge.ext.context.url.internal;
+package com.liferay.faces.bridge.ext.internal;
 
 import java.lang.reflect.Method;
 
