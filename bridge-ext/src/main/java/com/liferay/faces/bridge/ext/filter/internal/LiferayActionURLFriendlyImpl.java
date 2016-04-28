65f211d6acc765cd5ca1cb7a714a0f3cf1f9e226
Neil Griffin <neil.griffin.scm@gmail.com>
2016-04-28 19:58:41 -0400

    FACES-2654 Support Liferay Portal FriendlyURL for ActionURL and ResourceURL

---
+++ bridge-ext/src/main/java/com/liferay/faces/bridge/ext/filter/internal/LiferayActionURLFriendlyImpl.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.bridge.ext.filter.internal;
+
+import javax.faces.FacesWrapper;
+import javax.portlet.PortletURL;
+
+
+/**
+ * @author  Kyle Stiemann
+ */
+public class LiferayActionURLFriendlyImpl extends LiferayPortletURLFriendlyImpl implements LiferayActionURL,
+	FacesWrapper<PortletURL> {
+
+	// Private Members
+	private String responseNamespace;
+	private PortletURL wrappedLiferayPortletURL;
+
+	public LiferayActionURLFriendlyImpl(PortletURL wrappedLiferayPortletURL, String responseNamespace) {
+		this.wrappedLiferayPortletURL = wrappedLiferayPortletURL;
+		this.responseNamespace = responseNamespace;
+	}
+
+	@Override
+	protected LiferayURLGenerator getLiferayURLGenerator() {
+
+		PortletURL actionURL = getWrapped();
+
+		return new LiferayURLGeneratorActionImpl(actionURL.toString(), actionURL.getPortletMode(), responseNamespace,
+				actionURL.getWindowState());
+	}
+
+	@Override
+	public PortletURL getWrapped() {
+		return wrappedLiferayPortletURL;
+	}
+}
