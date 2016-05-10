86a5a1290ee2d2c3598a0a1876f9b93155c02f2c
Neil Griffin <neil.griffin.scm@gmail.com>
2016-05-10 16:59:12 -0400

    FACES-2653 Introduce new com.liferay.faces.bridge.ext sub-package and eliminate liferay sub-packages

--- bridge-ext/src/main/java/com/liferay/faces/bridge/ext/context/internal/PortalContextBridgeLiferayImpl.java
+++ bridge-ext/src/main/java/com/liferay/faces/bridge/ext/context/internal/PortalContextBridgeLiferayImpl.java
@@ -11,7 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-package com.liferay.faces.bridge.context.liferay.internal;
+package com.liferay.faces.bridge.ext.context.internal;
 
 import java.util.Enumeration;
 
