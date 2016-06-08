bd1931b512fce1ac0a4f658d440f53935b76b002
Neil Griffin <neil.griffin.scm@gmail.com>
2016-06-08 14:16:41 -0400

    FACES-2684 Add BridgePortletConfigFactory and BridgePortletContextFactory to the Bridge API

--- src/main/java/com/liferay/faces/bridge/ext/filter/internal/BridgePortletContextFactoryLiferayImpl.java
+++ src/main/java/com/liferay/faces/bridge/ext/filter/internal/BridgePortletContextFactoryLiferayImpl.java
@@ -15,7 +15,7 @@ package com.liferay.faces.bridge.ext.filter.internal;
 
 import javax.portlet.PortletContext;
 
-import com.liferay.faces.bridge.filter.BridgePortletContextFactory;
+import javax.portlet.faces.filter.BridgePortletContextFactory;
 
 
 /**
