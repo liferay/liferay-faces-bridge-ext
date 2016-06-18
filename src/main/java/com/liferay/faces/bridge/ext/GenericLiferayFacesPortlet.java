3fdd2f4cd6f73cee926cd96fd32e96a400ac8328
Neil Griffin <neil.griffin.scm@gmail.com>
2016-06-18 19:22:35 -0400

    FACES-2763 Remove the GenericLiferayFacesPortlet.doHeaders(RenderRequest,RenderResponse) method since it is redundant

--- src/main/java/com/liferay/faces/bridge/ext/GenericLiferayFacesPortlet.java
+++ src/main/java/com/liferay/faces/bridge/ext/GenericLiferayFacesPortlet.java
@@ -19,7 +19,6 @@ import javax.portlet.PortletException;
 import javax.portlet.PortletMode;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
-import javax.portlet.faces.Bridge;
 import javax.portlet.faces.GenericFacesPortlet;
 
 import com.liferay.portal.kernel.portlet.LiferayPortletMode;
@@ -62,35 +61,6 @@ public class GenericLiferayFacesPortlet extends GenericFacesPortlet {
 		}
 		else {
 			super.doDispatch(renderRequest, renderResponse);
-		}
-	}
-
-	@Override
-	protected void doHeaders(RenderRequest renderRequest, RenderResponse renderResponse) {
-
-		try {
-
-			// Streaming portals like WebSphere (as opposed to buffered portals like Liferay) will set the
-			// javax.portlet.render_part request attribute to a value of "RENDER_HEADERS" which will cause
-			// javax.portlet.GenericPortlet (the superclass of this class) to call this doHeaders(RenderRequest,
-			// RenderResponse) method, but will not in turn call GenericPortlet.doDispatch(RenderRequest,
-			// RenderResponse). That also means that that the doView(RenderRequest, RenderResponse) will not be called
-			// in this class. So if the attribute is set, we call the Bridge.doFacesRequest(RenderRequest,
-			// RenderResponse) method here, so that the Faces lifecycle can be run, and resources added to
-			// h:head can be retrieved. Note that it is the responsibility of the bridge to check for this
-			// attribute as well, because at this point the bridge should not render any JSF views to the response.
-			Object renderPartAttribute = renderRequest.getAttribute(RenderRequest.RENDER_PART);
-
-			if ((renderPartAttribute != null) && renderPartAttribute.equals(RenderRequest.RENDER_HEADERS)) {
-				Bridge facesBridge = getFacesBridge(renderRequest, renderResponse);
-				facesBridge.doFacesRequest(renderRequest, renderResponse);
-			}
-		}
-		catch (PortletException e) {
-
-			// Unfortunately the signature for GenericPortlet.doHeaders(RenderRequest, RenderResponse) does not throw
-			// an exception, so we have no choice but to simply report any exceptions by printing the stacktrace.
-			e.printStackTrace();
 		}
 	}
 }
