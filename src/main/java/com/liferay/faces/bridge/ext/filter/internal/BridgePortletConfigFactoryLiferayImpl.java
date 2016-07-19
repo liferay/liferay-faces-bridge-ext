d838e72c5556d56d27b0eeb14ca5e991c7fe22af
Neil Griffin <neil.griffin.scm@gmail.com>
2016-07-19 14:22:18 -0400

    FACES-2819 I8nBridgeImpl throws NullPointerException when resource-bundle is omitted from portlet.xml in Liferay Portal 6.2 (ensure statelessness by eliminating state and implementing Serializable)

--- src/main/java/com/liferay/faces/bridge/ext/filter/internal/BridgePortletConfigFactoryLiferayImpl.java
+++ src/main/java/com/liferay/faces/bridge/ext/filter/internal/BridgePortletConfigFactoryLiferayImpl.java
@@ -1,10 +1,32 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 package com.liferay.faces.bridge.ext.filter.internal;
 
-import com.liferay.faces.bridge.filter.BridgePortletConfigFactory;
+import java.io.Serializable;
 
 import javax.portlet.PortletConfig;
 
-public class BridgePortletConfigFactoryLiferayImpl extends BridgePortletConfigFactory {
+import com.liferay.faces.bridge.filter.BridgePortletConfigFactory;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class BridgePortletConfigFactoryLiferayImpl extends BridgePortletConfigFactory implements Serializable {
+
+	// serialVersionUID
+	private static final long serialVersionUID = 3839303045007599701L;
 
 	// Private Data Members
 	private BridgePortletConfigFactory wrappedBridgePortletConfigFactory;
