7026b63df5ccd2893696a5ea66775b47cf79f5f8
Neil Griffin <neil.griffin.scm@gmail.com>
2017-02-02 18:36:11 -0500

    FACES-2696 Utilize Portlet 3.0 dynamic resource dependency feature

--- src/main/java/com/liferay/faces/bridge/ext/filter/internal/BridgePortletRequestFactoryLiferayImpl.java
+++ src/main/java/com/liferay/faces/bridge/ext/filter/internal/BridgePortletRequestFactoryLiferayImpl.java
@@ -19,6 +19,8 @@ import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.EventRequest;
 import javax.portlet.EventResponse;
+import javax.portlet.HeaderRequest;
+import javax.portlet.HeaderResponse;
 import javax.portlet.PortalContext;
 import javax.portlet.PortletConfig;
 import javax.portlet.RenderRequest;
@@ -67,6 +69,18 @@ public class BridgePortletRequestFactoryLiferayImpl extends BridgePortletRequest
 		PortalContext portalContext = new PortalContextBridgeLiferayImpl(eventRequest);
 
 		return new EventRequestBridgeLiferayImpl(eventRequest, eventResponse.getNamespace(), portletConfig,
+				portalContext);
+	}
+
+	@Override
+	public HeaderRequest getHeaderRequest(HeaderRequest headerRequest, HeaderResponse headerResponse,
+		PortletConfig portletConfig, BridgeConfig bridgeConfig) {
+
+		headerRequest = getWrapped().getHeaderRequest(headerRequest, headerResponse, portletConfig, bridgeConfig);
+
+		PortalContext portalContext = new PortalContextBridgeLiferayImpl(headerRequest);
+
+		return new HeaderRequestBridgeLiferayImpl(headerRequest, headerResponse.getNamespace(), portletConfig,
 				portalContext);
 	}
 
