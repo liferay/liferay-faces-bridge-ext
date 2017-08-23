8c0fdd9ba3a0709a39fa1e0a0d6d98a96c701e34
Kyle Stiemann <stiemannkj1@gmail.com>
2017-08-23 11:56:28 -0400

    FACES-3183 Cache filtered resources in a thread-safe Map contained in the ServletContext

--- bridge-ext/src/main/java/com/liferay/faces/bridge/ext/application/internal/JSResourceWithDisabledAMDLoaderImpl.java
+++ bridge-ext/src/main/java/com/liferay/faces/bridge/ext/application/internal/JSResourceWithDisabledAMDLoaderImpl.java
@@ -13,9 +13,17 @@
  */
 package com.liferay.faces.bridge.ext.application.internal;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
 import javax.faces.application.Resource;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 import com.liferay.faces.util.application.FilteredResourceBase;
+import com.liferay.faces.util.application.ResourceUtil;
+import com.liferay.faces.util.cache.Cache;
 
 
 /**
@@ -31,13 +39,76 @@ public class JSResourceWithDisabledAMDLoaderImpl extends FilteredResourceBase {
 	}
 
 	@Override
+	public InputStream getInputStream() throws IOException {
+
+		InputStream inputStream = null;
+		Cache<String, String> filteredResourceCache = getFilteredResourceCache();
+
+		if (filteredResourceCache != null) {
+
+			String filteredResourceString = filteredResourceCache.getValue(getResourceId());
+
+			if (filteredResourceString != null) {
+				inputStream = ResourceUtil.toInputStream(filteredResourceString, getEncoding());
+			}
+		}
+
+		if (inputStream == null) {
+
+			// This method is implemented by FilteredResourceBase and calls the filter() method which caches the
+			// filtered resource string.
+			inputStream = super.getInputStream();
+		}
+
+		return inputStream;
+	}
+
+	@Override
 	public Resource getWrapped() {
 		return wrappedResource;
 	}
 
 	@Override
 	protected String filter(String string) {
-		return string.replaceAll("typeof\\s+define\\s*=(=+)\\s*[\"']function[\"']", "false&&typeof define=$1'function'")
-			.replaceAll("[\"']function[\"']\\s*=(=+)\\s*typeof\\s+define", "false&&'function'=$1typeof define");
+
+		String filteredResourceString = null;
+
+		Cache<String, String> filteredResourceCache = getFilteredResourceCache();
+		String resourceId = getResourceId();
+
+		if (filteredResourceCache != null) {
+			filteredResourceString = filteredResourceCache.getValue(resourceId);
+		}
+
+		if (filteredResourceString == null) {
+
+			filteredResourceString = string.replaceAll("typeof\\s+define\\s*=(=+)\\s*[\"']function[\"']",
+					"false&&typeof define=$1'function'").replaceAll("[\"']function[\"']\\s*=(=+)\\s*typeof\\s+define",
+					"false&&'function'=$1typeof define");
+
+			if (filteredResourceCache != null) {
+				filteredResourceString = filteredResourceCache.putValueIfAbsent(resourceId, filteredResourceString);
+			}
+		}
+
+		return filteredResourceString;
+	}
+
+	private Cache<String, String> getFilteredResourceCache() {
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = facesContext.getExternalContext();
+		Map<String, Object> applicationMap = externalContext.getApplicationMap();
+
+		return (Cache<String, String>) applicationMap.get(JSResourceWithDisabledAMDLoaderImpl.class.getName());
+	}
+
+	private String getResourceId() {
+
+		Resource wrappedResource = getWrapped();
+		String libraryName = wrappedResource.getLibraryName();
+		String resourceName = wrappedResource.getResourceName();
+
+		return ResourceUtil.getResourceId(libraryName, resourceName);
 	}
 }
