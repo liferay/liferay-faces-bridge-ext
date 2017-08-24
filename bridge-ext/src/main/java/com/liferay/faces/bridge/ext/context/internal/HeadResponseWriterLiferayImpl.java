8c1272197d823a373dd381437c18d6ff54d269f5
Neil Griffin <neil.griffin.scm@gmail.com>
2017-08-24 16:29:42 -0400

    FACES-3173 - Exclude **/internal/** packages from javadoc generation (code review)

--- bridge-ext/src/main/java/com/liferay/faces/bridge/ext/context/internal/HeadResponseWriterLiferayImpl.java
+++ bridge-ext/src/main/java/com/liferay/faces/bridge/ext/context/internal/HeadResponseWriterLiferayImpl.java
@@ -40,8 +40,8 @@ import com.liferay.taglib.util.HtmlTopTag;
 
 
 /**
- * Custom {@link ResponseWriter} that has the ability to write to the 'head' section of the portal page via
- * the Liferay vendor-specific mechanism.
+ * Custom {@link ResponseWriter} that has the ability to write to the &lt;head&gt;...&lt;/head&gt; section of the portal
+ * page via the Liferay vendor-specific mechanism.
  *
  * @author  Neil Griffin
  */
