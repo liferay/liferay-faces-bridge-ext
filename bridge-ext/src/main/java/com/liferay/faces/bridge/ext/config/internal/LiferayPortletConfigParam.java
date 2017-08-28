2a4aa974e3d58d23dd4012b7e7ed07e0450f8d04
Neil Griffin <neil.griffin.scm@gmail.com>
2017-08-28 17:27:42 -0400

    FACES-3183 Cache filtered resources in a thread-safe Map contained in the ServletContext (backport comments and utility code in order to minimize diffs across branches) (reverted from commit cd077ad470caf08a19e0f1b303605c149535c8a3)

--- bridge-ext/src/main/java/com/liferay/faces/bridge/ext/config/internal/LiferayPortletConfigParam.java
+++ bridge-ext/src/main/java/com/liferay/faces/bridge/ext/config/internal/LiferayPortletConfigParam.java
@@ -31,8 +31,8 @@ public enum LiferayPortletConfigParam implements ConfigParam<PortletConfig> {
 	 */
 	DistinctRequestScopedManagedBeans("com.liferay.faces.bridge.distinctRequestScopedManagedBeans", false),
 
-	/** Default value for Liferay Portal 6.2 is false, default value for 7.0+ is true. */
-	RequestDispatcherForwardEnabled("com.liferay.faces.bridge.requestDispatcherForwardEnabled", true),
+	// Default value for Liferay Portal 6.2 is false, default value for 7.0 (and above) is true.
+	RequestDispatcherForwardEnabled("com.liferay.faces.bridge.requestDispatcherForwardEnabled", false),
 
 	/**
 	 * Comma-delimited list of [library:resource] values for which the AMD loader should be disabled in addition to the
