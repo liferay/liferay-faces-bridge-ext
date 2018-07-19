0f1881ec90a3d6c5f42112ff501825bdf3c00602
Kyle Stiemann <stiemannkj1@gmail.com>
2018-07-19 17:51:43 -0400

    FACES-3322 Gradle build file versions are inconsistent with maven versions in archetypes

--- archetype/generate-and-deploy-test-archetype-portlets.sh
+++ archetype/generate-and-deploy-test-archetype-portlets.sh
@@ -5,6 +5,7 @@ buildArchetypeAndGenerateAndDeployTestArchetypePortlet() {
 	ARCHETYPE=$1
 	LIFERAY_VERSION=$2
 	JSF_VERSION=$3
+	BUILD_TOOL=$4
 
 	echo "Building $ARCHETYPE archetype:"
 	cd $ARCHETYPE
@@ -26,22 +27,37 @@ buildArchetypeAndGenerateAndDeployTestArchetypePortlet() {
 		-DinteractiveMode=false
 	cd com.mycompany.my.$ARCHETYPE_LIBRARY.portlet
 
-	echo "Building test $ARCHETYPE:"
-	mvn clean package
+	if [[ "$BUILD_TOOL" == "maven" ]]; then
+		echo "Building test $ARCHETYPE:"
+		mvn clean package || { echo "Failed to build portlet from $ARCHETYPE."; exit 1; }
 
-	echo "Deploying test $ARCHETYPE:"
-	cp target/*.war \
-		$HOME/Portals/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION/deploy/com.mycompany.my.$ARCHETYPE_LIBRARY.portlet.war
+		echo "Deploying test $ARCHETYPE:"
+		cp target/*.war \
+			$HOME/Portals/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION/deploy/com.mycompany.my.$ARCHETYPE_LIBRARY.portlet.war
+	elif [[ "$BUILD_TOOL" == "gradle" ]]; then
+		echo "Building test $ARCHETYPE:"
+		gradle clean build || { echo "Failed to build portlet from $ARCHETYPE."; exit 1; }
+
+		echo "Deploying test $ARCHETYPE:"
+		cp build/libs/*.war \
+			$HOME/Portals/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION/deploy/com.mycompany.my.$ARCHETYPE_LIBRARY.portlet.war
+	fi
 }
 
 mvn clean
 mkdir -p ./target/
 
-LIFERAY_VERSION=$(cd ../bridge-ext && mvn org.codehaus.mojo:exec-maven-plugin:1.2.1:exec -Dexec.executable="echo" \
+BUILD_TOOL="maven"
+
+if [[ "$1" == "gradle" ]]; then
+	BUILD_TOOL="gradle"
+fi
+
+LIFERAY_VERSION=$(mvn org.codehaus.mojo:exec-maven-plugin:1.2.1:exec -Dexec.executable="echo" \
 	-q --non-recursive \
 	-Dexec.args='${liferay.version}')
 
-JSF_VERSION=$(cd ../bridge-ext && mvn org.codehaus.mojo:exec-maven-plugin:1.2.1:exec -Dexec.executable="echo" \
+JSF_VERSION=$(mvn org.codehaus.mojo:exec-maven-plugin:1.2.1:exec -Dexec.executable="echo" \
 	-q --non-recursive \
 	-Dexec.args='${faces.api.version}')
 
@@ -49,9 +65,10 @@ JSF_VERSION=$(cd ../bridge-ext && mvn org.codehaus.mojo:exec-maven-plugin:1.2.1:
 if hash parallel 2>/dev/null; then
 	export -f buildArchetypeAndGenerateAndDeployTestArchetypePortlet
 	parallel --no-notice --max-args=1 \
-		buildArchetypeAndGenerateAndDeployTestArchetypePortlet {} $LIFERAY_VERSION $JSF_VERSION ::: ./*-portlet
+		buildArchetypeAndGenerateAndDeployTestArchetypePortlet {} $LIFERAY_VERSION $JSF_VERSION $BUILD_TOOL ::: \
+			./*-portlet
 else
 	for ARCHETYPE in ./*-portlet; do
-		(buildArchetypeAndGenerateAndDeployTestArchetypePortlet $ARCHETYPE $LIFERAY_VERSION $JSF_VERSION)
+		(buildArchetypeAndGenerateAndDeployTestArchetypePortlet $ARCHETYPE $LIFERAY_VERSION $JSF_VERSION $BUILD_TOOL)
 	done
 fi
