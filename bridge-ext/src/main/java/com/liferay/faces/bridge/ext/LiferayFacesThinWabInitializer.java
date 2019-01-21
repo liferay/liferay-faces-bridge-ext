dcf1a108898b2349924a9382b4254fa3c493266b
Kyle Stiemann <stiemannkj1@gmail.com>
2019-01-21 18:11:11 -0500

    FACES-3024 Enable deployment of Liferay Faces dependencies as OSGi bundles (avoid exception(s) when OSGi weaver is deployed after Faces bundles)

--- bridge-ext/src/main/java/com/liferay/faces/bridge/ext/LiferayFacesThinWabInitializer.java
+++ bridge-ext/src/main/java/com/liferay/faces/bridge/ext/LiferayFacesThinWabInitializer.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2018 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2019 Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -14,15 +14,19 @@
 package com.liferay.faces.bridge.ext;
 
 import java.util.EventListener;
+import java.util.Iterator;
+import java.util.ServiceLoader;
 import java.util.Set;
 
+import javax.portlet.faces.Bridge;
 import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+import org.osgi.annotation.versioning.ProviderType;
+
 import com.liferay.faces.util.osgi.FacesThinWabInitializer;
 import com.liferay.faces.util.osgi.OSGiClassLoaderUtil;
-import org.osgi.annotation.versioning.ProviderType;
 
 
 /**
@@ -31,22 +35,41 @@ import org.osgi.annotation.versioning.ProviderType;
 @ProviderType
 public final class LiferayFacesThinWabInitializer implements ServletContainerInitializer {
 
+	private static boolean isBridgeImplementationAvailable() {
+
+		boolean bridgeImplAvailable = false;
+		ServiceLoader<Bridge> serviceLoader = ServiceLoader.load(Bridge.class);
+
+		if (serviceLoader != null) {
+
+			Iterator<Bridge> iterator = serviceLoader.iterator();
+
+			bridgeImplAvailable = (iterator.hasNext() && (iterator.next() != null));
+		}
+
+		return bridgeImplAvailable;
+	}
+
 	@Override
 	public void onStartup(Set<Class<?>> classes, ServletContext servletContext) throws ServletException {
 
-		FacesThinWabInitializer facesThinWabInitializer = new FacesThinWabInitializer();
-		facesThinWabInitializer.onStartup(classes, servletContext);
+		// Only run the thin wab initializer when a Bridge implementation is available.
+		if (isBridgeImplementationAvailable()) {
 
-		try {
+			FacesThinWabInitializer facesThinWabInitializer = new FacesThinWabInitializer();
+			facesThinWabInitializer.onStartup(classes, servletContext);
 
-			Class<?> thisClass = this.getClass();
-			Class<? extends EventListener> bridgeSessionListenerClass = (Class<? extends EventListener>)
-				OSGiClassLoaderUtil.classForName("com.liferay.faces.bridge.servlet.BridgeSessionListener",
-					servletContext, thisClass);
-			servletContext.addListener(bridgeSessionListenerClass);
-		}
-		catch (ClassNotFoundException e) {
-			throw new ServletException(e);
+			try {
+
+				Class<?> thisClass = this.getClass();
+				Class<? extends EventListener> bridgeSessionListenerClass = (Class<? extends EventListener>)
+					OSGiClassLoaderUtil.classForName("com.liferay.faces.bridge.servlet.BridgeSessionListener",
+						servletContext, thisClass);
+				servletContext.addListener(bridgeSessionListenerClass);
+			}
+			catch (ClassNotFoundException e) {
+				throw new ServletException(e);
+			}
 		}
 	}
 }
