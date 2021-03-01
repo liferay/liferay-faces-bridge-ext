497f33262c816e14b4d5836c14264cefd30d7e0a
Neil Griffin <neil.griffin.scm@gmail.com>
2021-03-01 17:21:06 -0500

    FACES-3598 Add a "thin" Maven profile for each Liferay Faces archetype in order to support JSF thin WAR deployment (Added comment that informs the developer how CDI is activated)

--- archetype/jsf-portlet/src/main/resources/archetype-resources/src/main/java/com/mycompany/backing/MyBacking.java
+++ archetype/jsf-portlet/src/main/resources/archetype-resources/src/main/java/com/mycompany/backing/MyBacking.java
@@ -7,6 +7,8 @@ import javax.enterprise.context.RequestScoped;
 
 @Named
 @RequestScoped
+// CDI will only be activated if WEB-INF/beans.xml is present. If building with
+// Maven, use the "-P cdi" profile to generate the WEB-INF/beans.xml descriptor.
 public class MyBacking {
 
 	private Greeting greeting = new Greeting();
