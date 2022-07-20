60d35e46b936abad688230f58015827793e35406
Neil Griffin <neil.griffin.scm@gmail.com>
2022-07-20 13:39:33 -0400

    FACES-3598 Add a "thin" Maven profile for each Liferay Faces archetype in order to support JSF thin WAR deployment (script that generates and deploys the archetypes for testing)

---
+++ archetype/build-generate-deploy.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+ARCHETYPE_VERSION=$(mvn org.codehaus.mojo:exec-maven-plugin:1.2.1:exec -Dexec.executable="echo" \
+	-q --non-recursive \
+	-Dexec.args='${project.version}')
+echo "ARCHETYPE_VERSION=$ARCHETYPE_VERSION"
+
+JSF_VERSION=$(mvn org.codehaus.mojo:exec-maven-plugin:1.2.1:exec -Dexec.executable="echo" \
+	-q --non-recursive \
+	-Dexec.args='${faces.api.version}')
+echo "JSF_VERSION=$JSF_VERSION"
+
+LIFERAY_VERSION=$(mvn org.codehaus.mojo:exec-maven-plugin:1.2.1:exec -Dexec.executable="echo" \
+	-q --non-recursive \
+	-Dexec.args='${liferay.version}')
+echo "LIFERAY_VERSION=$LIFERAY_VERSION"
+
+PORTLET_VERSION=$(mvn org.codehaus.mojo:exec-maven-plugin:1.2.1:exec -Dexec.executable="echo" \
+	-q --non-recursive \
+	-Dexec.args='${portlet.api.version}')
+echo "PORTLET_VERSION=$PORTLET_VERSION"
+
+CDI=""
+if [[ "$@" == *"cdi"* ]]; then
+    CDI="cdi"
+fi
+echo "CDI=$CDI"
+
+THICK_OR_THIN="thick"
+if [[ "$@" == *"thin"* ]]; then
+    THICK_OR_THIN="thin"
+fi
+echo "THICK_OR_THIN=$THICK_OR_THIN"
+
+LIFERAY_HOME=$PORTALS_HOME/liferay.com/liferay-dxp-$LIFERAY_VERSION-portlet-$PORTLET_VERSION-jsf-$JSF_VERSION-$THICK_OR_THIN
+echo "LIFERAY_HOME=$LIFERAY_HOME"
+mkdir -p $LIFERAY_HOME/deploy
+
+# First, it is necessary to build from the top, so that pom.xml and parent/pom.xml get installed
+# Note that the externalLiferayFacesRepositories profile is necessary so that the gradle-tooling-api will get resolved
+mvn clean install -P externalLiferayFacesRepositories
+#mvn archetype:update-local-catalog -P externalLiferayFacesRepositories
+
+mkdir -p target
+pushd target
+
+PORTLET_TYPES="adf alloy bootsfaces butterfaces icefaces jsf primefaces richfaces"
+for PORTLET_TYPE in $PORTLET_TYPES; do
+	echo $PORTLET_TYPE
+	ARTIFACT_ID="com.mycompany.my.$PORTLET_TYPE.portlet"
+	rm -rf $ARTIFACT_ID
+	mvn --batch-mode archetype:generate \
+		-DarchetypeCatalog=local \
+		-DgroupId=com.mycompany \
+		-Dversion="1.0.0" \
+		-DartifactId=$ARTIFACT_ID \
+		-DarchetypeGroupId=com.liferay.faces.archetype \
+		-DarchetypeArtifactId=com.liferay.faces.archetype.$PORTLET_TYPE.portlet \
+		-DarchetypeVersion=$ARCHETYPE_VERSION \
+		-DinteractiveMode=false
+done
+
+mvn archetype:crawl
+
+for PORTLET_TYPE in $PORTLET_TYPES; do
+	echo $PORTLET_TYPE
+	ARTIFACT_ID="com.mycompany.my.$PORTLET_TYPE.portlet"
+	pushd $ARTIFACT_ID
+	mvn clean package -P $THICK_OR_THIN,$CDI
+	cp target/*.war $LIFERAY_HOME/deploy
+	popd
+done
+
+popd
+
+echo "WARs have been deployed to $LIFERAY_HOME/deploy"
